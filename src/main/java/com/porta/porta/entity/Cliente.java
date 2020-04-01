382d0f5f9b5189121bb39db235ff79c1d9214ce9
Marco <marcoastorga.g@gmail.com>
2020-03-31 22:34:04 -0300

    add cliente

--- src/main/java/com/porta/porta/entity/Cliente.java
+++ src/main/java/com/porta/porta/entity/Cliente.java
@@ -1,107 +1,99 @@
-// package com.porta.porta.entity;
+package com.porta.porta.entity;
+
+import javax.persistence.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Table(name = "cliente")
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id_cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id")
+    private User id_usuario;
+
+    @NotBlank
+    @Size(max = 40)
+    private String rut;
+    @NotBlank
+    @Size(max = 40)
+    private String nombre;
+    @NotBlank
+    @Size(max = 40)
+    private String apellido;
+    @NotBlank
+    @Size(max = 40)
+    private String telefono;
+    @NotBlank
+    @Size(max = 40)
+    private String fechaNacimiento;
+
+    public Cliente() {
+    }
+
+    public Long getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(Long id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public User getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(User id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 
 
-// import javax.persistence.*;
-
-// import javax.validation.constraints.NotBlank;
-// import javax.validation.constraints.Size;
-
-
-
-// import java.util.HashSet;
-// import java.util.Set;
-
-
-// @Table(name = "cliente")
-// @Entity
-// public class Cliente {
-    
-
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.AUTO)
-//     private Long id_cliente;
-
-//     @ManyToMany(fetch = FetchType.LAZY)
-//     @JoinTable(name = "users",
-//             joinColumns = @JoinColumn(name = "id"),
-//             inverseJoinColumns = @JoinColumn(name = "role_id"))
-//     private Set<User> idUsuario = new HashSet<>();
-
-//     @NotBlank
-//     @Size(max = 40)
-//     private String rut;
-//     @NotBlank
-//     @Size(max = 40)
-//     private String nombre;
-//     @NotBlank
-//     @Size(max = 40)
-//     private String apellido;
-//     @NotBlank
-//     @Size(max = 40)
-//     private String telefono;
-//     @NotBlank
-//     @Size(max = 40)
-//     private String fechaNacimiento;
-
-//     public Cliente() {
-//     }
-
-//     public Long getId_cliente() {
-//         return id_cliente;
-//     }
-
-//     public void setId_cliente(Long id_cliente) {
-//         this.id_cliente = id_cliente;
-//     }
-
-//     public Set<User> getIdUsuario() {
-//         return idUsuario;
-//     }
-
-//     public void setIdUsuario(Set<User> idUsuario) {
-//         this.idUsuario = idUsuario;
-//     }
-
-//     public String getRut() {
-//         return rut;
-//     }
-
-//     public void setRut(String rut) {
-//         this.rut = rut;
-//     }
-
-//     public String getNombre() {
-//         return nombre;
-//     }
-
-//     public void setNombre(String nombre) {
-//         this.nombre = nombre;
-//     }
-
-//     public String getApellido() {
-//         return apellido;
-//     }
-
-//     public void setApellido(String apellido) {
-//         this.apellido = apellido;
-//     }
-
-//     public String getTelefono() {
-//         return telefono;
-//     }
-
-//     public void setTelefono(String telefono) {
-//         this.telefono = telefono;
-//     }
-
-//     public String getFechaNacimiento() {
-//         return fechaNacimiento;
-//     }
-
-//     public void setFechaNacimiento(String fechaNacimiento) {
-//         this.fechaNacimiento = fechaNacimiento;
-//     }
-
-
-    
-// }
+}
