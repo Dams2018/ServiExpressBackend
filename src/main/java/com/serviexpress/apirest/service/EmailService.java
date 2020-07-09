6dd92277a2bfe6645e699728bf85d26257533bbe
Marco <marcoastorga.g@gmail.com>
2020-07-09 17:35:35 -0400

    add email encuesta fix other email

--- src/main/java/com/serviexpress/apirest/service/EmailService.java
+++ src/main/java/com/serviexpress/apirest/service/EmailService.java
@@ -3,4 +3,5 @@ package com.serviexpress.apirest.service;
 public interface EmailService {
     public void emailSend(String email,String name, String usuario, String pass);
     public void emailSendReserva(String email,String name, String mensaje, String estado);
+    public void emailSendEncuesta(String email,String name, String mensaje, String estado);
 }
