1f33d1a94de09b6f7727ad0995a6c6b81294eb04
Julio Jimenez <j.jimenezv098@gmail.com>
2020-07-09 17:58:41 -0400

    Fix

--- src/main/java/com/serviexpress/apirest/mapper/ReporteServicios.java
+++ src/main/java/com/serviexpress/apirest/mapper/ReporteServicios.java
@@ -11,7 +11,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface ReporteServicios {
     
-    @Select("SELECT count(s.idservicio)totalservios, TO_CHAR(fecha, 'mm/yyyy') mes, s.nombre "+
+    @Select("SELECT count(s.idservicio) as totalservicios, TO_CHAR(fecha, 'mm/yyyy') mes, s.nombre as nombreservicio "+
 	"FROM reserva r "+	
 	"INNER JOIN servicio s ON r.servicios = s.idservicio "+
 	"GROUP BY TO_CHAR(fecha, 'mm/yyyy'),s.nombre order by mes asc"
